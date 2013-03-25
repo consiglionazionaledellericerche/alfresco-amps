72daa5b1ce8567fa3ddddb051d41de872e3a10be
Marco Spasiano <marco.spasiano@cnr.it>
2013-03-25 13:28:30 +0000

    Gestione dei Gruppi
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/trunk/alfresco@2012 18059cec-d5c9-4679-bb3d-60c028e053f1

--- alfresco-amp/groups-extension/src/main/java/it/cnr/si/repo/jscript/GroupAuthority.java
+++ alfresco-amp/groups-extension/src/main/java/it/cnr/si/repo/jscript/GroupAuthority.java
@@ -136,9 +136,11 @@ public class GroupAuthority extends BaseScopableProcessorExtension {
         ParameterCheck.mandatoryString("AuthorityName", authorityName);
         AuthorityPermission authorityPermission = null;
         NodeRef authorityRef;
-        if (NodeRef.isNodeRef(authorityName))
+        if (NodeRef.isNodeRef(authorityName)){
         	authorityRef = new NodeRef(authorityName);
-        else	
+        	if (groupAuthorityService.getAuthorityContainer().equals(authorityRef))
+        		authorityRef = groupAuthorityService.getAuthorityContainer();
+        }else	
         	authorityRef = groupAuthorityService.getAuthorityNodeRefOrNull(authorityName);
         if (authorityRef != null)
         {
